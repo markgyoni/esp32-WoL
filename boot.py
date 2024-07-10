56876403916d08bb8726396e319a939393a677af
markgyoni <markgyoni@gmail.com>
2024-07-10 21:02:44 +0200

    start working on main webserver

--- boot.py
+++ boot.py
@@ -1,9 +1,21 @@
+# Connect to WiFi
+import network
+import env
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(env.WLAN_SSID, env.WLAN_PASSWORD)
+
 # WebREPL (8266)
 import webrepl
 webrepl.start()
 
-# Connect to WiFi
-import network
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect('Tbone  WiFi', 'paradicsom2022')
+import time
+pprefix = 'BOOT:'
+print()
+print(pprefix, 'WiFi connection success')
+time.sleep(1)
+print(pprefix, "IP:", wlan.ifconfig()[0])
+print()
+
+import gc
+gc.collect()
--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-import env
-import network
-import machine  # type: ignore
-import time
 import socket
 import binascii
-import requests  # type: ignore
+import network
+import env
 
+pprefix = "MAIN:"
 
-# def microdot_start():
-#     from microdot import Microdot
-
-#     app = Microdot()
-
-#     @app.route("/")
-#     async def index(request):
-#         return "Hello, world!"
-
-#     app.run(port=80)
-
-
-# microdot_start()
-
-
-# Connect to WiFi if not yet connected
-def wifi_connect():
+def wifi_connect():   
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
 
     if not wlan.isconnected():
         wlan.connect(env.WLAN_SSID, env.WLAN_PASSWORD)
+        print(pprefix, 'WiFi connection success')
+        print(pprefix, "IP:", wlan.ifconfig())
     else:
         pass
 
-
-def send_wol_packet(mac_address, broadcast_ip="255.255.255.255", port=9):
-    magic_packet = b"\xff" * 6 + (binascii.unhexlify(mac_address.replace(":", "")) * 16)
+def send_wol_packet(mac_address, broadcast_ip='255.255.255.255', port=9):
+    magic_packet = b'\xff' * 6 + (binascii.unhexlify(mac_address.replace(':', '')) * 16)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.sendto(magic_packet, (broadcast_ip, port))
     s.close()
-
 
 target_mac_address = env.MAC_ADDRESS
 
@@ -50,36 +32,86 @@ failure_count = 0
 
 wifi_connect()
 
+# WEBSITE
+
+try:
+  import usocket as socket # type: ignore
+except:
+  import socket
+
+from machine import Pin # type: ignore
+led = Pin(2, Pin.OUT)
+
+def web_page():
+
+  import uping
+  pingResult = uping.ping('192.168.1.250', count=2)
+  pcstatus = ""
+  if pingResult[1] == 0:
+      print("PC offline")
+      pcstatus = "Off"
+  else:
+      print("PC online")
+      pcstatus = "On"
+  
+  html = """<html>
+   <head>
+      <title>ESP32-WoL</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="data:,">
+      <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+         h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+         border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+         .button2{background-color: #4286f4;}
+      </style>
+   </head>
+   <body>
+      <h1>Computer status</h1>
+      <p>State: <strong>""" + pcstatus + """</strong></p>
+      <p><a href="/?led=on"><button class="button">ON</button></a></p>
+      <p><a href="/?led=off"><button class="button button2">OFF</button></a></p>
+      <script>
+        function toggleLED() {
+          fetch('/toggle')
+            .then(response => response.json())
+            .then(data => {
+              if (data.led_state === 1) {
+                document.getElementById('led-status').innerText = 'On';
+              } else {
+                document.getElementById('led-status').innerText = 'Off';
+              }
+            });
+        }
+      </script>
+      <p>LED Status: <strong id="led-status">""" + ('On' if led.value() == 1 else 'Off') + """</strong></p>
+      <p><button class="button" onclick="toggleLED()">Toggle LED</button></p>
+   </body>
+</html>"""
+
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 80))
+s.listen(5)
+
 while True:
-    time.sleep(5)
-
-    body = {}
-
-    try:
-        body = requests.get(
-            f"https://api.telegram.org/{env.TELEGRAM_BOT_TOKEN}/getUpdates?offset={offset + 1}"
-        ).json()
-    except:
-        print("Unable to fetch updates")
-        continue
-
-    updates = body["result"]
-
-    for update in updates:
-        offset = update["update_id"]
-
-        if (
-            update["message"]["from"]["id"] in authorized
-            and update["message"]["text"] == "/wol"
-        ):
-            try:
-                send_wol_packet(target_mac_address)
-
-                requests.get(
-                    f'https://api.telegram.org/{env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={update["message"]["from"]["id"]}&text=Sent%20WoL%20packet.'
-                )
-            except:
-                print("Unable to send WoL or message.")
-
-    if failure_count >= 5:
-        machine.reset()
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+  led_on = request.find('/?led=on')
+  led_off = request.find('/?led=off')
+  if led_on == 6:
+    print('LED ON')
+    led.value(1)
+  if led_off == 6:
+    print('LED OFF')
+    led.value(0)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
